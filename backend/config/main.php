79e539c8f0372c65fb6d2eb3e5b62cf1d193afaa
Armo <mro1127@163.com>
2017-12-26 11:55:06 +0800

    主语言：中文

--- backend/config/main.php
+++ backend/config/main.php
@@ -8,6 +8,7 @@ $params = array_merge(
 
 return [
     'id' => 'app-backend',
+    'language' => 'zh-CN',
     'basePath' => dirname(__DIR__),
     'controllerNamespace' => 'backend\controllers',
     'bootstrap' => ['log'],
