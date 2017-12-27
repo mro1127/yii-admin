2c1bbd1c1052b50f7f11145dcf3f1f4bdbf94929
Armo <mro1127@163.com>
2017-12-27 18:27:34 +0800

    配置文件

---
+++ backend/config/main-local.php
@@ -0,0 +1,27 @@
+<?php
+
+$config = [
+    'components' => [
+        'request' => [
+            // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
+            'cookieValidationKey' => 'r3PICL-TNW_hGs7Qky2Fl7t81kyst_NN',
+        ],
+    ],
+];
+
+if (!YII_ENV_TEST) {
+    // configuration adjustments for 'dev' environment
+    $config['bootstrap'][] = 'debug';
+    $config['modules']['debug'] = [
+        'class' => 'yii\debug\Module',
+        'allowedIPs'=> ['192.168.114.1','::1']
+    ];
+
+    $config['bootstrap'][] = 'gii';
+    $config['modules']['gii'] = [
+        'class' => 'yii\gii\Module',
+        'allowedIPs'=> ['192.168.114.1','::1']
+    ];
+}
+
+return $config;
---
+++ common/config/main-local.php
@@ -0,0 +1,14 @@
+<?php
+return [
+    'components' => [
+        'mailer' => [
+            'class' => 'yii\swiftmailer\Mailer',
+            'viewPath' => '@common/mail',
+            // send all mails to a file by default. You have to set
+            // 'useFileTransport' to false and configure a transport
+            // for the mailer to send real emails.
+            'useFileTransport' => true,
+        ],
+        'db' => require(__DIR__ . '/db.php'),
+    ],
+];
