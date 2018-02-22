7d279790eefa76f4318972c33691b380e5d8fbbe
Armo <mro1127@163.com>
2018-02-22 22:13:04 +0800

    默认home页，啥都没有的

---
+++ backend/views/site/home.php
@@ -0,0 +1,18 @@
+<?php 
+use yii\helpers\Url;
+
+ ?>
+<div class="">
+home
+</div>
+<!-- ./wrapper -->
+<?php $this->on($this::EVENT_END_PAGE, function () {    ?>
+
+<script>
+    $(function() {
+
+    })
+
+</script>
+
+<?php }); ?>
