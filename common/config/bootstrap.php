a75926795453dd1da8a0d9809a9d3ce66f48afa7
git <mro1127@163.com>
2018-09-21 12:13:36 +0800

    新增storage

--- common/config/bootstrap.php
+++ common/config/bootstrap.php
@@ -8,4 +8,12 @@ Yii::setAlias('@frontend', dirname(dirname(__DIR__)) . '/frontend');
 Yii::setAlias('@backend', dirname(dirname(__DIR__)) . '/backend');
 Yii::setAlias('@console', dirname(dirname(__DIR__)) . '/console');
 Yii::setAlias('@static', './static');
+Yii::setAlias('@storage', realpath(__DIR__ . '/../../storage'));
 Yii::setAlias('@commonStatic', dirname(dirname(__DIR__)) . '/common/static');
+
+
+Yii::setAlias('@frontendUrl', env('FRONTEND_HOST_INFO') . env('FRONTEND_BASE_URL'));
+Yii::setAlias('@backendUrl', env('BACKEND_HOST_INFO') . env('BACKEND_BASE_URL'));
+Yii::setAlias('@storageUrl', env('STORAGE_HOST_INFO') . env('STORAGE_BASE_URL'));
+
+
