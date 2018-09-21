2e396e51980ef814855094ae2cc852b86ab5677f
git <mro1127@163.com>
2018-09-21 12:49:00 +0800

    统一配置名

--- storage/web/index.php
+++ storage/web/index.php
@@ -12,6 +12,6 @@ require(__DIR__ . '/../../vendor/yiisoft/yii2/Yii.php');
 require(__DIR__ . '/../../common/config/bootstrap.php');
 
 // App Config
-$config = require(__DIR__ . '/../config/base.php');
+$config = require(__DIR__ . '/../config/main.php');
 
 (new yii\web\Application($config))->run();
