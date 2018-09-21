17e6b9b2edc4dda5b9e1253ba319ed6877cead0d
git <mro1127@163.com>
2018-09-21 12:03:13 +0800

    引入.env配置

--- backend/web/index.php
+++ backend/web/index.php
@@ -1,18 +1,15 @@
 <?php
-ini_set("error_reporting","E_ALL & ~E_NOTICE");
-defined('YII_DEBUG') or define('YII_DEBUG', true);
-defined('YII_ENV') or define('YII_ENV', 'dev');
-
 require __DIR__ . '/../../vendor/autoload.php';
+
+// Environment
+require(__DIR__ . '/../../common/env.php');
+
 require __DIR__ . '/../../vendor/yiisoft/yii2/Yii.php';
 require __DIR__ . '/../../common/config/bootstrap.php';
 require __DIR__ . '/../config/bootstrap.php';
 
 $config = yii\helpers\ArrayHelper::merge(
     require __DIR__ . '/../../common/config/main.php',
-    require __DIR__ . '/../../common/config/main-local.php',
-    require __DIR__ . '/../config/main.php',
-    require __DIR__ . '/../config/main-local.php'
+    require __DIR__ . '/../config/main.php'
 );
-
 (new yii\web\Application($config))->run();
--- common/helpers.php
+++ common/helpers.php
@@ -27,3 +27,30 @@ if (YII_DEBUG) {
         die;
     }
 }
+
+/**
+ * @param string $key
+ * @param mixed $default
+ * @return mixed
+ */
+function env($key, $default = null)
+{
+
+    $value = getenv($key) ?? $_ENV[$key] ?? $_SERVER[$key];
+
+    if ($value === false) {
+        return $default;
+    }
+
+    switch (strtolower($value)) {
+        case 'true':
+        case '(true)':
+            return true;
+
+        case 'false':
+        case '(false)':
+            return false;
+    }
+
+    return $value;
+}
