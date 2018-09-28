db1a403028e1794f268d6a5a1213ce1e3f7c816f
git <mro1127@163.com>
2018-09-29 00:21:11 +0800

    配置基础路劲

--- common/config/bootstrap.php
+++ common/config/bootstrap.php
@@ -3,13 +3,14 @@
 require_once(__DIR__ . '/../helpers.php');
 
 // 别名
+Yii::setAlias('@base', realpath(__DIR__ . '/../../'));
 Yii::setAlias('@common', dirname(__DIR__));
 Yii::setAlias('@frontend', dirname(dirname(__DIR__)) . '/frontend');
 Yii::setAlias('@backend', dirname(dirname(__DIR__)) . '/backend');
 Yii::setAlias('@console', dirname(dirname(__DIR__)) . '/console');
 Yii::setAlias('@static', './static');
-Yii::setAlias('@storage', realpath(__DIR__ . '/../../storage'));
 Yii::setAlias('@commonStatic', dirname(dirname(__DIR__)) . '/common/static');
+Yii::setAlias('@storage', realpath(__DIR__ . '/../../storage'));
 
 
 Yii::setAlias('@frontendUrl', env('FRONTEND_HOST_INFO') . env('FRONTEND_BASE_URL'));
