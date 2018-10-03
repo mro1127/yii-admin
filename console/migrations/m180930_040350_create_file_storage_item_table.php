271a76f4a31cd0918902113648847b08853718ab
git <mro1127@163.com>
2018-10-03 12:34:06 +0800

    文件表字段长度

--- console/migrations/m180930_040350_create_file_storage_item_table.php
+++ console/migrations/m180930_040350_create_file_storage_item_table.php
@@ -14,13 +14,13 @@ class m180930_040350_create_file_storage_item_table extends Migration
     {
         $this->createTable('file_storage_item', [
             'id'         => $this->primaryKey(),
-            'component'  => $this->string(20)->notNull(),
-            'base_url'   => $this->string(20)->comment('基础路径')->notNull(),
-            'path'       => $this->string(20)->comment('路径')->notNull(),
-            'type'       => $this->string(20)->comment('类型'),
+            'component'  => $this->string(255)->notNull(),
+            'base_url'   => $this->string(1024)->comment('基础路径')->notNull(),
+            'path'       => $this->string(1024)->comment('路径')->notNull(),
+            'type'       => $this->string(255)->comment('类型'),
             'size'       => $this->integer(11)->comment('大小'),
-            'name'       => $this->string(20)->comment('文件名'),
-            'upload_ip'  => $this->string(20)->comment('上传IP'),
+            'name'       => $this->string(255)->comment('文件名'),
+            'upload_ip'  => $this->string(15)->comment('上传IP'),
             'created_at' => $this->integer(11)->comment('添加时间')->notNull(),
         ]);
         $this->addCommentOnTable('file_storage_item', '上传附件表');
