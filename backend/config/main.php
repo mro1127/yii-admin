fbb98d0fba2e30c6679aaec990da7db330849f7f
Armo <mro1127@gmail.com>
2018-11-06 00:00:11 +0800

    yii-debug工具

--- backend/config/main.php
+++ backend/config/main.php
@@ -86,13 +86,13 @@ if (YII_ENV_DEV) {
     $config['bootstrap'][] = 'debug';
     $config['modules']['debug'] = [
         'class' => 'yii\debug\Module',
-        'allowedIPs'=> ['192.168.114.1','::1']
+        'allowedIPs'=> ['::1','*']
     ];
 
     $config['bootstrap'][] = 'gii';
     $config['modules']['gii'] = [
         'class' => 'yii\gii\Module',
-        'allowedIPs'=> ['192.168.114.1','::1']
+        'allowedIPs'=> ['::1','*']
     ];
 }
 return $config;
